93887fe88a4ae0194e0d3f8bf42c30128acfe7dd
faodiallo001 <faodiallo001@gmail.com>
2026-06-25 14:41:38 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ const mobileMenu = document.getElementById("mobileMenu");
 menuToggle.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
 });
+const heroSlides = document.querySelectorAll(".hero-slide");
+
+let currentHeroSlide = 0;
+
+setInterval(() => {
+  heroSlides[currentHeroSlide].classList.remove("active");
+
+  currentHeroSlide++;
+
+  if (currentHeroSlide >= heroSlides.length) {
+    currentHeroSlide = 0;
+  }
+
+  heroSlides[currentHeroSlide].classList.add("active");
+}, 5000);
